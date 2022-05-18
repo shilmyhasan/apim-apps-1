04cbad9d8a5a424833d4626ca2bad1a61cd4c02f
dhanushka <madushandhanushka@gmail.com>
2022-05-18 11:02:54 +0400

    Add test.sh file

---
+++ tests/test.sh
@@ -0,0 +1,168 @@
+#!/bin/bash
+echo "tesh.sh executing started..."
+
+set -o xtrace
+
+HOME=`pwd`
+TEST_SCRIPT=test.sh
+
+function usage()
+{
+    echo "
+    Usage bash test.sh --input-dir /workspace/data-bucket.....
+    Following are the expected input parameters. all of these are optional
+    --input-dir       | -i    : input directory for test.sh
+    --output-dir      | -o    : output directory for test.sh
+    "
+}
+
+optspec=":hiom-:"
+while getopts "$optspec" optchar; do
+    case "${optchar}" in
+        -)
+            case "${OPTARG}" in
+                input-dir)
+                    val="${!OPTIND}"; OPTIND=$(( $OPTIND + 1 ))
+                    INPUT_DIR=$val
+                    ;;
+                output-dir)
+                    val="${!OPTIND}"; OPTIND=$(( $OPTIND + 1 ))
+                    OUTPUT_DIR=$val
+                    ;;
+                mvn-opts)
+                    val="${!OPTIND}"; OPTIND=$(( $OPTIND + 1 ))
+                    MAVEN_OPTS=$val
+                    ;;
+                *)
+                    usage
+                    if [ "$OPTERR" = 1 ] && [ "${optspec:0:1}" != ":" ]; then
+                        echo "Unknown option --${OPTARG}" >&2
+                    fi
+                    ;;
+            esac;;
+        h)
+            usage
+            exit 2
+            ;;
+        o)
+            OUTPUT_DIR=$val
+            ;;
+        m)
+            MVN_OPTS=   $val
+            ;;
+        i)
+            INPUT_DIR=$val
+            ;;
+        *)
+            usage
+            if [ "$OPTERR" != 1 ] || [ "${optspec:0:1}" = ":" ]; then
+                echo "Non-option argument: '-${OPTARG}'" >&2
+            fi
+            ;;
+    esac
+done
+
+echo "working Directory : ${HOME}"
+echo "input directory : ${INPUT_DIR}"
+echo "output directory : ${OUTPUT_DIR}"
+export DATA_BUCKET_LOCATION=${INPUT_DIR}
+
+# Retrieve specific property from deployment.properties file
+function get_prop {
+    local prop=$(grep -w "${1}" "${INPUT_DIR}/deployment.properties" | cut -d'=' -f2)
+    echo $prop
+}
+
+PRODUCT_VERSION=$(get_prop 'ProductVersion')
+
+if [[ -z "$PRODUCT_VERSION" ]]
+then
+    echo "\$ProductVersion not found in property list."
+#    After merging changes to wso2/testgrid-job-configs this need to be enabled
+#    exit 1
+else
+    PRODUCT_VERSION="-$PRODUCT_VERSION"
+fi
+
+######
+export DEBIAN_FRONTEND=noninteractive
+sudo apt-get update -y
+sleep 300
+sudo killall apt apt-get dpkg
+sudo dpkg --configure -a
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+npm -v
+if [[ $? -ne 0 ]]
+then
+    echo "NPM exists and removing existing version."
+    sudo apt-get purge nodejs -y
+    sudo apt-get purge npm -y
+else
+    echo "NPM Deos NOT exists and installing existing version."
+fi
+
+wget https://nodejs.org/dist/v12.22.3/node-v12.22.3-linux-x64.tar.xz
+tar -xvf node-v12.22.3-linux-x64.tar.xz
+sudo ln -s $HOME/node-v12.22.3-linux-x64/bin/node /usr/bin/node
+sudo ln -s $HOME/node-v12.22.3-linux-x64/bin/npm /usr/bin/npm
+sudo ln -s $HOME/node-v12.22.3-linux-x64/bin/npx /usr/bin/npx
+sudo apt-get install libgtk2.0-0 libgtk-3-0 libgbm-dev libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2 libxtst6 xauth xvfb -y
+export LC_CTYPE="en_US.UTF-8"
+cd $HOME
+npm install cypress
+npm install --save-dev cypress-file-upload
+npm install --save  nodemailer
+npm install --save require-text
+npm install styliner
+npm i --save-dev cypress-mochawesome-reporter
+npm i --save-dev mocha-junit-reporter
+npm i --save-dev cypress-multi-reporters
+VAR=`grep "PublisherUrl" ../../../../data-bucket/deployment.properties |cut -d'=' -f2`
+VAR2=${VAR//[\\]}
+export CYPRESS_BASE_URL=${VAR2//\/publisher}
+
+VAR3=`grep "s3secretKey" ../../../../data-bucket/deployment.properties |cut -d'=' -f2`
+export S3_SECRET_KEY=${VAR3}
+VAR4=`grep "s3accessKey" ../../../../data-bucket/deployment.properties |cut -d'=' -f2`
+export S3_ACCESS_KEY=${VAR4}
+
+VAR5=`grep "TEST_PLAN_ID" ../../../../data-bucket/deployment.properties |cut -d'=' -f2`
+export TEST_PLAN_ID=${VAR5}
+
+VAR6=`grep "TESTGRID_EMAIL_PASSWORD" ../../../../data-bucket/deployment.properties | head -1 | cut -d'=' -f2`
+export TESTGRID_EMAIL_PASSWORD=${VAR6}
+
+npm install --save-dev cypress-multi-reporters mocha-junit-reporter
+npm install --save-dev mochawesome mochawesome-merge mochawesome-report-generator
+npm install --save-dev mocha
+npm install junit-report-merger --save-dev
+npm i --save aws-sdk
+npm run delete:reportFolderHTML
+npm run delete:reportFolderJUnit
+npm run delete:reportFolderReport
+npm run pre-test
+npm run test
+npm run report:merge
+npm run report:generate
+mv  ./cypress/reports/html/mochawesome-bundle.html  ./cypress/reports/html/mochawesome-bundle-${TEST_PLAN_ID}.html
+node ./upload_email
+
+######
+
+mvn clean install
+
+echo `pwd`
+#=============== Copy Surefire Reports ===========================================
+
+echo "Copying surefire-reports to ${OUTPUT_DIR}/scenarios"
+mkdir -p ${OUTPUT_DIR}/scenarios
+mv ${OUTPUT_DIR}/scenarios/target/* ${OUTPUT_DIR}/scenarios/
+find . -name "surefire-reports" -exec cp --parents -r {} ${OUTPUT_DIR}/scenarios \;
+find . -name "aggregate-surefire-report" -exec cp --parents -r {} ${OUTPUT_DIR}/scenarios \;
+
+#=============== Code Coverage Report Generation ===========================================
+
+echo "Generating Scenario Code Coverage Reports"
+source ${HOME}/code-coverage/code-coverage.sh
+
+generate_code_coverage ${INPUT_DIR} ${OUTPUT_DIR}
---
+++ tests/upload_email.js
@@ -0,0 +1,71 @@
+const fs = require('fs');
+const AWS = require('aws-sdk');
+
+var nodemailer = require('nodemailer');
+
+// Enter copied or downloaded access ID and secret key here
+const ID = '';
+const SECRET = '';
+
+// The name of the bucket that you have created
+const BUCKET_NAME = 'apim-3.2.0-ui-testing';
+var secretAccessKey = process.env.S3_SECRET_KEY;
+var accessKeyId = process.env.S3_ACCESS_KEY;
+var testPlanId = process.env.TEST_PLAN_ID;
+var testGridEmailPWD = process.env.TESTGRID_EMAIL_PASSWORD;
+
+
+const s3 = new AWS.S3({
+  accessKeyId: accessKeyId,
+  secretAccessKey: secretAccessKey
+});
+
+const uploadFile = (fileName) => {
+  // Read content from the file
+  const fileContent = fs.readFileSync(fileName);
+
+  // Setting up S3 upload parameters
+  const params = {
+      Bucket: BUCKET_NAME,
+      Key: 'mochawesome-bundle-' + testPlanId + '.html', // File name you want to save as in S3
+      Body: fileContent,
+      ACL: 'public-read',
+      ContentType : "text/html"
+  };
+
+  // Uploading files to the bucket
+  s3.upload(params, function(err, data) {
+      if (err) {
+          // throw err;
+          console.log(`File uploaded Error to AWS s3 bucket.`);
+      }
+      console.log(`File uploaded successfully. ${data.Location}`);
+
+      var content = `Click on ${data.Location} to view the complete test report`;
+      var transporter = nodemailer.createTransport({
+        host: 'tygra.wso2.com',
+        port: '2587',
+        auth: {
+          user: 'testgrid',
+          pass: testGridEmailPWD
+        }
+      });
+
+      var mailOptions = {
+          from: "TestGrid Team <testgrid@gmail.com>",
+          to: "prasanna@wso2.com,vimukthi@wso2.com,rosens@wso2.com,nandika@wso2.com",
+          subject: `WSO2 APIM 3.2.0 UI TESTS ${testPlanId}`,
+          html: content
+      }
+
+
+      transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+      });
+  });
+};
+uploadFile('./cypress/reports/html/mochawesome-bundle-' + testPlanId + '.html');
