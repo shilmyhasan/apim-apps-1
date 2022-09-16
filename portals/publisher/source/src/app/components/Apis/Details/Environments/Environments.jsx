ff15ad3aa3efa1a421f2efcc2ac6c35fa0c13ebc
GihanAyesh <gihanayeshmanthadrck@gmail.com>
2022-09-16 12:26:25 +0530

    Remove hardcoding Gateway Access URL

--- portals/publisher/source/src/app/components/Apis/Details/Environments/Environments.jsx
+++ portals/publisher/source/src/app/components/Apis/Details/Environments/Environments.jsx
@@ -1388,10 +1388,12 @@ export default function Environments() {
         }
 
         const httpContext = vhost.httpContext ? '/' + vhost.httpContext.replace(/^\//g, '') : '';
-        endpoints.primary = 'http://' + vhost.host
-            + (vhost.httpPort === 80 ? '' : ':' + vhost.httpPort) + httpContext;
-        endpoints.secondary = 'https://' + vhost.host
-            + (vhost.httpsPort === 443 ? '' : ':' + vhost.httpsPort) + httpContext;
+        if (vhost.httpPort !== 80) {
+            endpoints.primary = 'http://' + vhost.host + ':' + vhost.httpPort + httpContext;
+        }
+        if (vhost.httpsPort !== 443) {
+            endpoints.secondary = 'https://' + vhost.host + ':' + vhost.httpsPort + httpContext;
+        }
         endpoints.combined = endpoints.secondary + ' ' + endpoints.primary;
         return endpoints;
     }
