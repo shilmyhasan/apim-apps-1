1aef38f05a12e4f6cf9300cbad7d8f8829f42ad1
Thisal Tennakoon <42693357+thisaltennakoon@users.noreply.github.com>
2023-07-11 20:27:45 +0530

    Update 09-add-edit-delete-km.spec.js

--- tests/cypress/integration/admin/09-add-edit-delete-km.spec.js
+++ tests/cypress/integration/admin/09-add-edit-delete-km.spec.js
@@ -22,8 +22,8 @@ describe("Add key manager", () => {
 
     const { carbonUsername, carbonPassword, tenant, superTenant } = Utils.getUserInfo();
 
-    const addKeyManager = (usernameLocal, passwordLocal, tenant) => {
-        cy.loginToAdmin(usernameLocal, passwordLocal, tenant);
+    const addKeyManager = (usernameLocal, passwordLocal) => {
+        cy.loginToAdmin(usernameLocal, passwordLocal);
         const km = 'myAuth0';
         const wellKnowUrl = 'https://my-tenant.us.auth0.com/.well-known/openid-configuration';
         const clientId = 'test';
@@ -93,9 +93,9 @@ describe("Add key manager", () => {
         cy.get('td > div').contains(km).should('not.exist');
     }
     it.only("Add key manager - super admin", () => {
-        addKeyManager(carbonUsername, carbonPassword, superTenant);
+        addKeyManager(carbonUsername, carbonPassword);
     });
     it.only("Add key manager - tenant user", () => {
-        addKeyManager(Utils.getTenantUser(carbonUsername, tenant), carbonPassword, tenant);
+        addKeyManager(Utils.getTenantUser(carbonUsername, tenant), carbonPassword);
     });
 })
